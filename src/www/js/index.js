2fb5c7e2e443fcbd58e7cb7b246b53170beaf1c1
jeremie-arcidiacono <49244181+jeremie-arcidiacono@users.noreply.github.com>
2023-02-01 14:55:30 +0100

    Init commit

--- src/www/js/index.js
+++ src/www/js/index.js
@@ -1,10 +1,6 @@
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
