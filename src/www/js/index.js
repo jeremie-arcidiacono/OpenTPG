805c3fec83920f715d67378dce50e0594bff1a6f
jeremie-arcidiacono <49244181+jeremie-arcidiacono@users.noreply.github.com>
2023-02-15 13:20:36 +0100

    Add network availability check

--- src/www/js/index.js
+++ src/www/js/index.js
@@ -1,6 +1,26 @@
-document.addEventListener('deviceready', onDeviceReady, false);
+function onLoad() {
+    document.addEventListener('deviceready', onDeviceReady, false);
+}
 
+// Cordova is ready at this point
 function onDeviceReady() {
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    document.addEventListener('resume', onResume, false);
+
+    ensureNetworkAvailability();
+}
+
+
+function onResume() {
+    ensureNetworkAvailability();
+}
+
+
+/*
+ * Check if the network is available.
+ * If not, display an alert.
+*/
+function ensureNetworkAvailability() {
+    if (navigator.connection.type == Connection.NONE) {
+        alert('No network connection');
+    }
 }
