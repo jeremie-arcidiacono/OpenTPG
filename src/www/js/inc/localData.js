5892410fc6d882f02d1b62d0fda35c16f8b46973
jeremie-arcidiacono <49244181+jeremie-arcidiacono@users.noreply.github.com>
2023-03-01 14:13:58 +0100

    Remove Promise for local Station getter

--- src/www/js/inc/localData.js
+++ src/www/js/inc/localData.js
@@ -5,29 +5,47 @@ const REMOTE_STATIONS_FILE_URL = 'https://raw.githubusercontent.com/jeremie-arci
 class LocalData {
 
     /**
+     * Get a Station object from the local storage
      * @param {string} name
-     * @return {Promise<Station>}
+     * @return {Station|null}
      */
-    getStationByName(name) {
-        return new Promise((resolve, reject) => {
-            let stations = JSON.parse(localStorage.getItem('stations'))["stations"];
+    static getStationByName(name) {
+        let stations = JSON.parse(localStorage.getItem('stations'))["stations"];
 
-            // Convert the stations object to an array
-            let stationsArray = Object.keys(stations).map(function (key) {
-                return [key, stations[key]];
-            });
-
-            let station = stationsArray.find(station => station[1].name === name);
-
-            if (station !== null) {
-                resolve(new Station(station[0], station[1].name, station[1].lines));
-            } else {
-                reject('Station not found');
-            }
+        // Convert the stations object to an array
+        let stationsArray = Object.keys(stations).map(function (key) {
+            return [key, stations[key]];
         });
+
+        let station = stationsArray.find(station => station[1].name === name);
+
+        if (station !== null) {
+            return new Station(station[0], station[1].name, station[1].lines);
+        }
+        return null;
     }
 
-    updateStationStorage() {
+    /**
+     * Get a Station object from the local storage
+     * @param {string} id
+     * @return {Station|null}
+     */
+    static getStationById(id) {
+        let stations = JSON.parse(localStorage.getItem('stations'))["stations"];
+
+        if (stations[id] !== null) {
+            return new Station(id, stations[id].name, stations[id].lines);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Put the stations data from a remote location.
+     * If the local data is older than the remote data, update the local data.
+     * @return {Promise<unknown>}
+     */
+    static updateStationStorage() {
         return new Promise((resolve, reject) => {
 
             // Get the remote file
