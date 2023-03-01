8b0f0caac46bf5fe13fb11ba4a78001fbe4c48e0
jeremie-arcidiacono <49244181+jeremie-arcidiacono@users.noreply.github.com>
2023-03-01 09:37:12 +0100

    Create Station class

---
+++ src/www/js/inc/station.js
@@ -0,0 +1,18 @@
+"use strict";
+
+export class Station {
+
+    /**
+     * 
+     * @param {string} id The id of the station
+     * @param {string} name The name of the station
+     * @param {string[]} lines Array of string representing the lines number
+     * @param {boolean} isFavorite True if the user has marked the station as favorite
+     */
+    constructor(id, name, lines, isFavorite=false) {
+        this.id = id;
+        this.name = name;
+        this.lines = lines;
+        this.isFavorite = isFavorite;
+    }
+}
